b039a26665b9460c8eb519094f70c66efba1de99
Afza Adawiyah <b031910063@student.utem.edu.my>
2021-03-18 21:10:31 +0800

    Initial Commit

---
+++ Lab1/src/exercise7/HumanWeightInMars.java
@@ -0,0 +1,17 @@
+package exercise7;
+
+public class HumanWeightInMars {
+
+	public static void main(String[] args) {
+		
+		double weight = 76.7;
+		double weightInMars;
+		
+		weightInMars = weight*0.3783;
+		
+		System.out.print("Human Weigth in Earth: " + weight);
+		System.out.print("\nHuman Weigth in Mars: " + weightInMars);
+
+	}
+
+}
