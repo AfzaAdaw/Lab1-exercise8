effde65250cec952649a2a743b4c95f2ead4f874
Afza Adawiyah <b031910063@student.utem.edu.my>
2021-03-18 20:43:35 +0800

    add more attributes

--- samples/samples/demo/Location.java
+++ samples/samples/demo/Location.java
@@ -2,11 +2,21 @@ package samples.demo;
 
 public class Location {
 	private int age;
+	private String name;
+	
 	public void setAge(int age) {
-		
+		this.age = age;
 	}
 	
 	public int getAge() {
 		return age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
